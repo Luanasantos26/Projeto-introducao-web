e84835278f31eac8d0486f087bd83d3306f62a5e
Luanasantos26 <luana26lourdes@gmail.com>
2023-02-06 18:02:49 -0300

    semana 2

--- index.js
+++ index.js
@@ -1,68 +1,29 @@
-/*const animal = {
-    nome: 'cachorro',
-    nome_cientifico: 'Canis lupus familiaris',
-    idade_media: '12 anos',
-    classe: 'mamifero',
-    ambiente: 'domestico',
-    console.log('está extinto?')
-}*/
-
-//1
-/*const nome = prompt("informe o Nome ou apelido do cachorro: ")
+/*const nome = prompt("informe o Nome ou apelido do animal: ")
 const idade = prompt("Informa a idade do animal: ")
+const classe = prompt("Informe a classe: ")
 let ambiente = prompt("Qual ambiente o animal vive? ")
-var extinto = false*/
+var extinto = false
 
-
-/*console.log("Nome: "+ nome)
+console.log("Nome: "+ nome)
 console.log("idade media: "+ idade)
 console.log("classe: Mamifero")
 console.log("Ambiente "+ ambiente)*/
+/*_____________________________________________________________________________________________________________*/
+//1 objeto
+/*const animal = {
+    nome: "Cachorro",
+    idade: 12,
+    classe: "mamífero",
+    ambiente: "domestico",
+    extinto: false
+}*/
 
-/*2
-const nome = "Fifi"
-const idade = "11 anos"
-const classe = "Mamifero"
-let ambiente = "Domestico"
+//2 array
+//var array = []
 
-const nome = "Molico"
-const idade = "9 anos"
-const classe = "Mamifero"
-let ambiente = "Domestico"
-
-const nome = "Silvinho"
-const idade = "38 anos"
-const classe = "Ovíparos"
-let ambiente = "Lagoa"*/
-
-/*3.
-const valor1 = 20
-const valor2 = 30
-const valor3 = 40
-const soma = valor1 + valor2 + valor3
-const media = soma/3
-console.log("A média dos 3 valores é: ", media)*/
+//3 add obj no array (push)
+var array = []
+array.push({nome:"cachorro", idade:12, classe:"mamifero", ambiente:"domestico", extinto: false})
+console.log(array)
 
 //4
-console.log("É verdade que está extinto: ", typeof extinto)
-//5
-
-/*6
-console.log (toUpperCase(nome))
-console.log("idade media: "+ idade)
-console.log("classe: Mamifero")
-console.log("Classe: Mamifero")
-console.log("Ambiente " + ambiente)*/
-
-/*Semana 2
-atividade 1
-const animal = {
-   nome: "Gato",
-   nome_cientifico: "Felis catus",
-   idade_media: "12 anos",
-   classe: "mamifero",
-   ambiente: "domestico",
-   vida:("Não está extinto.")
-}
-atividade 2*/
-
